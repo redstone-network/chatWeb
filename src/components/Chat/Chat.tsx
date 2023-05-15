7a7eafa491ab3e193057fc1422936a56d863e19e
李云飞 <liyunfei02@daojia-inc.com>
2023-05-15 20:18:42 +0800

    feat: 代码优化

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,59 +1,11 @@
 import React from 'react';
-import useStore from '@store/store';
-
 import ChatContent from './ChatContent';
-import MobileBar from '../MobileBar';
-
+import ChatHeader from './ChatHeader';
 const Chat = () => {
-  const hideSideMenu = useStore((state) => state.hideSideMenu);
-  const chatTitle = useStore((state) =>
-    state.chats &&
-    state.chats.length > 0 &&
-    state.currentChatIndex >= 0 &&
-    state.currentChatIndex < state.chats.length
-      ? state.chats[state.currentChatIndex].title
-      : 'New Chat'
-  );
+
   return (
-    <div
-      className={`flex h-full flex-1 flex-col ${
-        hideSideMenu ? 'md:pl-0' : 'md:pl-80'
-      }`}
-    >
-      <MobileBar />
-      <header className='h-12 bg-white/80 backdrop-blur-lg fixed top-0 left-0 right-0 flex px-3 items-center border-b justify-between shrink-0 gap-5 z-10 transition-transform md:ml-80'>
-        <div className='flex items-center'>
-          <button
-            type='button'
-            className='inline-flex items-center justify-center -ml-2 shrink-0 h-8 w-8 rounded-lg'
-          >
-            <span className='i-tabler-menu-2'></span>
-          </button>
-          <span className='line-clamp-1'>{chatTitle}</span>
-        </div>
-        <div className='flex items-center gap-2 shrink-0'>
-          <button
-            type='button'
-            aria-haspopup='dialog'
-            aria-expanded='false'
-            aria-controls='radix-:r8:'
-            data-state='closed'
-            className='rounded-lg px-2 text-sm inline-flex items-center justify-center border shrink-0 text-center disabled:pointer-events-none border-zinc-300 bg-white border-b-zinc-400/80 active:bg-zinc-200 h-8 text-logStart font-semibold'
-          >
-            <span className='mr-1 i-ion-diamond-outline text-lg'></span>
-          </button>
-          <button
-            type='button'
-            aria-haspopup='dialog'
-            aria-expanded='false'
-            aria-controls='radix-:rb:'
-            data-state='closed'
-            className='rounded-lg px-2 inline-flex items-center justify-center border shrink-0 text-center disabled:pointer-events-none border-zinc-300 bg-white border-b-zinc-400/80 active:bg-zinc-200 h-8 w-8 text-lg'
-          >
-            <span className='i-ion-ios-settings-strong'></span>
-          </button>
-        </div>
-      </header>
+    <div className={`flex h-full flex-1 flex-col md:pl-80`}>
+      <ChatHeader/>
       <main className='relative h-full w-full transition-width flex flex-col overflow-hidden items-stretch flex-1 pt-12'>
         <ChatContent />
       </main>
