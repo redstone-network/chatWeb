0c62087fad1ed1a8c316d3e6208fdc42200c0b27
李云飞 <liyunfei02@daojia-inc.com>
2023-05-18 14:01:48 +0800

    feat: 适配手机端

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,44 +1,38 @@
 import LogoIcon from '@icon/LogoIcon';
 import HomeBG from '@src/assets/background.png';
+import ChatDataInsight from '@src/assets/ChatDataInsight1.png';
+import Title from '@src/assets/title1.png';
+import Desc from '@src/assets/desc1.png';
+
 export default function name() {
   const goToBeta = () => {
-    window.location.href = 'https://forms.gle/2sLg51Q6tfavRqDMA';
+    window.location.href = 'https://forms.gle/VXmvY3S7yN7torWT8';
   };
   return (
     <div className='bg-white min-h-screen flex flex-col'>
       <div className='flex items-center justify-between'>
         <div className='flex items-center pt-7'>
-          <div className='w-20 mr-7 ml-10'>
+          <div className='sm:w-20 mr-7 ml-10 w-16'>
             <LogoIcon />
           </div>
           <div className='text-homeMain font-urbanist text-3xl font-extrabold'>
-            ChatData Insight
+            <img className="sm:w-60 w-40" src={ ChatDataInsight} />
           </div>
-        </div>
-        <div className='pr-14'>
-          {/* <button className='text-homeMain border-solid border-2 rounded-md	 px-16 py-2 border-homeMain font-urbanist text-2xl'>
-            Sign In
-          </button> */}
         </div>
       </div>
-      <div className='flex-grow'>
-        <div className='min-h-full pt-32'>
-          <div className='font-urbanist text-6xl text-left font-blob pb-14 pl-24'>
-            <p>Your trusted </p>
-            <p className='bg-gradient-to-r text-homeMain'>decision-making</p>
-            <p>assistant</p>
+      <div className='flex-grow bg-HomeBG sm:bg-[length:65%] bg-[length:100%] bg-no-repeat bg-right-bottom'>
+        <div className='min-h-full sm:pt-32 pt-16'>
+          <div className='font-urbanist text-6xl text-left font-blob sm:pb-14 sm:pl-24 pb-10 pl-8'>
+          <img className="sm:h-56 h-28" src={ Title } />
           </div>
-          <div className='font-urbanist text-3xl text-left font-medium	pb-24  pl-24'>
-            Unlock the power of data <br /> with ChatGPT
+          <div className='font-urbanist text-3xl text-left font-medium	sm:pb-24  sm:pl-24 pb-10 pl-8'>
+          <img className="sm:h-20 h-10" src={ Desc } />
           </div>
-          <div className='pl-24'>
-            <button onClick={goToBeta} className='text-white font-semibold	 text-3xl rounded-2xl bg-gradient-to-l from-btnStart to-btnEnd px-14 py-2'>
+          <div className='sm:pl-24 pl-8'>
+            <button onClick={goToBeta} className='text-white font-semibold	 sm:text-3xl text-lg rounded-2xl bg-gradient-to-l from-btnStart to-btnEnd px-14 py-2'>
               Join Beta
             </button>
           </div>
-        </div>
-        <div className="absolute w-8/12 bottom-0	right-0">
-          <img src={HomeBG} alt="" />
         </div>
       </div>
     </div>
--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -3,15 +3,7 @@ import { Line } from '@ant-design/charts';
 
 const Page: React.FC = () => {
   const data = [
-    { year: '1991', value: 3 },
-    { year: '1992', value: 4 },
-    { year: '1993', value: 3.5 },
-    { year: '1994', value: 5 },
-    { year: '1995', value: 4.9 },
-    { year: '1996', value: 6 },
-    { year: '1997', value: 7 },
-    { year: '1998', value: 9 },
-    { year: '1999', value: 13 },
+    { x: '1991', y: 3 }
   ];
 
   const config = {
@@ -20,8 +12,8 @@ const Page: React.FC = () => {
       visible: true,
       text: '带数据点的折线图',
     },
-    xField: 'year',
-    yField: 'value',
+    xField: 'x',
+    yField: 'y',
   };
   return <Line {...config} />;
 };
