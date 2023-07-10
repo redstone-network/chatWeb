c3266dfc5c0cbf7ad5d553404b0f0f583a321c49
李云飞 <liyunfei02@daojia-inc.com>
2023-07-11 00:09:35 +0800

    feat: css

--- src/components/Menu/NewChat.tsx
+++ src/components/Menu/NewChat.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Avatar from '@icon/Avatar';
+import Avatar from '@src/assets/user.png';
 
 const NewChat = () => {
   return (
-    <div className='border-t shrink-0 px-4 py-6 relative'>
+    <div className='border-t shrink-0 px-4 pt-6 pb-5 relative'>
       <div className='right-4	bg-gradient-to-tr text-sm from-btnStart to-btnEnd absolute text-white py- text-center rounded py-0.5 px-2'>
         Free
       </div>
       <div className='flex items-center pb-4'>
         <div className='mr-2'>
-          <Avatar />
+          <img className="w-10 h-10" src={Avatar} alt="" />
         </div>
         <div>
           <div className='text-black text-base'>User</div>
