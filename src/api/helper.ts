51fe759b9783127c86f619eff6e643c0284dbe83
李云飞 <liyunfei02@daojia-inc.com>
2023-07-30 14:24:32 +0800

    fix: bug

--- src/api/helper.ts
+++ src/api/helper.ts
@@ -7,10 +7,7 @@ export const parseEventSource = (
     .split('\n\n')
     .filter(Boolean)
     .map((chunk) => {
-      const jsonString = chunk
-        .split('\n')
-        .map((line) => line.replace(/^data: /, ''))
-        .join('');
+      const jsonString = chunk;
       if (jsonString === '[DONE]') return jsonString;
       try {
         const json = JSON.parse(jsonString);
